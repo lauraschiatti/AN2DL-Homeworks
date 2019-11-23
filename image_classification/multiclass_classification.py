a48ea8d359369fa31648039b39643b34d6fc136d
lauricdd <lauri_cdd@hotmail.com>
2019-11-23 11:26:33 +0100

    removing tabs

--- image_classification/multiclass_classification.py
+++ image_classification/multiclass_classification.py
@@ -5,9 +5,8 @@ import tensorflow as tf
 
 import utils.data_loader as data
 
-
 # -------------------------------------- #
-	### Multi-class Classification ###
+### Multi-class Classification ###
 # -------------------------------------- #
 
 
@@ -15,7 +14,6 @@ import utils.data_loader as data
 # ----------------------------------
 seed = 123
 tf.random.set_seed(seed)
-
 
 # todo: gpu management
 # Set GPU memory growth
@@ -27,7 +25,6 @@ tf.random.set_seed(seed)
 train_generator, valid_generator, test_generator = data.setup_data_generator()
 
 data.show_batch(train_generator)
-
 
 # Create model
 # ------------
@@ -43,33 +40,27 @@ x = tf.keras.Input(shape=[256, 256])  # input tensor
 
 flatten = tf.keras.layers.Flatten()(x)
 h = tf.keras.layers.Dense(units=10, activation=tf.keras.activations.sigmoid)(flatten)  # hidden layers
-#output layer:probabccc of belonging to each class
+# output layer:probabccc of belonging to each class
 out = tf.keras.layers.Dense(units=10, activation=tf.keras.activations.softmax)(h)
 model = tf.keras.Model(inputs=x, outputs=out)
-
-
 
 # Visualize created model as a table
 model.summary()
 print('model initial weights', model.weights)
 
-
-
 # Specify the training configuration (optimizer, loss, metrics)
 # -------------------------------------------------------------
-loss = tf.keras.losses.CategoricalCrossentropy() # loss function to minimize
+loss = tf.keras.losses.CategoricalCrossentropy()  # loss function to minimize
 
-lr = 1e-4 # learning rate
-optimizer = tf.keras.optimizers.Adam(learning_rate=lr) # stochastic gradient descent optimizer
+lr = 1e-4  # learning rate
+optimizer = tf.keras.optimizers.Adam(learning_rate=lr)  # stochastic gradient descent optimizer
 
-metrics = ['accuracy'] # validation metrics to monitor
+metrics = ['accuracy']  # validation metrics to monitor
 
 # Compile Model
 model.compile(optimizer=optimizer,
-				loss=loss,
-				metrics=metrics)
-
-
+              loss=loss,
+              metrics=metrics)
 
 # Train the model
 # ---------------
@@ -80,13 +71,13 @@ model.compile(optimizer=optimizer,
 # step_size_valid = valid_gen.n // valid_gen.batch_size
 #
 # fit_model = model.fit_generator(generator= train_gen,
-# 								steps_per_epoch=step_size_train,
-# 								epochs=epochs,
-# 								validation_data=valid_gen, # validation generator
-# 								validation_steps=step_size_valid)
-# 								# callbacks=[checkpointer, stopper]
-# 							  	# shuffle=True)
-
+#                                 steps_per_epoch=step_size_train,
+#                                 epochs=epochs,
+#                                 validation_data=valid_gen, # validation generator
+#                                 validation_steps=step_size_valid)
+#                                 # callbacks=[checkpointer, stopper]
+#                                   # shuffle=True)
+#
 
 # example
 # model.fit_generator(
@@ -97,23 +88,21 @@ model.compile(optimizer=optimizer,
 #     epochs = nb_epochs)
 
 fit_model = model.fit_generator(train_generator,
-								steps_per_epoch=2000,
-								epochs=50,
-								validation_data=valid_generator,
-								validation_steps=800)
-
+                                steps_per_epoch=2000,
+                                epochs=50,
+                                validation_data=valid_generator,
+                                validation_steps=800)
 
 # history lds a record of the loss values and metric values during training
 print('\nhistory dict:', fit_model.history)
-
 
 # Evaluate the model
 # ----------
 
 # print('Evaluate model on test data ... ')
 # eval_out = model.evaluate_generator(generator=valid_gen,
-# 									 	steps=step_size_valid)
-#										verbose=0)
+#                                          steps=step_size_valid)
+#                                        verbose=0)
 
 # print('test loss, test acc:', eval_out)
 
@@ -147,15 +136,15 @@ print('\nhistory dict:', fit_model.history)
 # --------------
 
 # step_size_test = test_gen.n // test_gen.batch_size
-    
+
 
 # reset the test_generator before whenever you call the predict_generator.
 # This is important, if you forget to reset the test_generator you will get outputs in a weird order.
 # test_gen.reset()
 #
 # pred = model.predict_generator(generator=test_gen,
-# 									steps=step_size_test,
-# 									verbose=1)
+#                                     steps=step_size_test,
+#                                     verbose=1)
 #
 #
 # predicted_class_indices=np.argmax(pred,axis=1) # predicted abels
@@ -175,7 +164,5 @@ print('\nhistory dict:', fit_model.history)
 # image_filenames = test_gen.filenames
 #
 # results = pd.DataFrame({"Filename": image_filenames,
-# 						   "Predictions": predictions})
+#                            "Predictions": predictions})
 # results.to_csv("results.csv", index=False)
-
-
--- image_classification/utils/data_loader.py
+++ image_classification/utils/data_loader.py
@@ -6,7 +6,6 @@ import tensorflow as tf
 import split_folders
 from keras.preprocessing.image import ImageDataGenerator
 
-
 # fix the seed for random operations to make experiments reproducible
 
 seed = 123
@@ -20,16 +19,13 @@ dataset_dir = os.path.join(cwd, 'image_classification/dataset')
 input_dir = os.path.join(dataset_dir, 'training')
 dataset_split_dir = os.path.join(dataset_dir, 'dataset_split')
 
-
-valid_split = 0.2 # fraction of images reserved for validation
+valid_split = 0.2  # fraction of images reserved for validation
 
 # split into training and validation sets using a ratio . e.g. for train/val `.8 .2`.
 split_folders.ratio(input_dir,
-					output=dataset_split_dir,
-					seed=seed, # allows to reproduce the split
-					ratio=(1-valid_split, valid_split))
-
-
+                    output=dataset_split_dir,
+                    seed=seed,  # allows to reproduce the split
+                    ratio=(1 - valid_split, valid_split))
 
 # todo: create config dic with params
 
@@ -54,206 +50,200 @@ bs = 32  # (default)
 # number of classes
 num_classes = 20
 
-class_list = ['owl',  				# 0
-			  'galaxy',  			# 1
-			  'lightning',  		# 2
-			  'wine-bottle',  		# 3
-			  't-shirt',  			# 4
-			  'waterfall',  		# 5
-			  'sword',  			# 6
-			  'school-bus',  		# 7
-			  'calculator',  		# 8
-			  'sheet-music',  		# 9
-			  'airplanes',  		# 10
-			  'lightbulb',  		# 11
-			  'skyscraper',  		# 12
-			  'mountain-bike',  	# 13
-			  'fireworks',  		# 14
-			  'computer-monitor',  	# 15
-			  'bear',  				# 16
-			  'grand-piano',  		# 17
-			  'kangaroo',  			# 18
-			  'laptop']  			# 19
-
+class_list = ['owl',  # 0
+              'galaxy',  # 1
+              'lightning',  # 2
+              'wine-bottle',  # 3
+              't-shirt',  # 4
+              'waterfall',  # 5
+              'sword',  # 6
+              'school-bus',  # 7
+              'calculator',  # 8
+              'sheet-music',  # 9
+              'airplanes',  # 10
+              'lightbulb',  # 11
+              'skyscraper',  # 12
+              'mountain-bike',  # 13
+              'fireworks',  # 14
+              'computer-monitor',  # 15
+              'bear',  # 16
+              'grand-piano',  # 17
+              'kangaroo',  # 18
+              'laptop']  # 19
 
 
 # Create image generators from directory
 # --------------------------------------
 def setup_data_generator():
-	apply_data_augmentation = False
+    apply_data_augmentation = False
 
-	# define data augmentation configuration
+    # define data augmentation configuration
 
-	if apply_data_augmentation:
+    if apply_data_augmentation:
 
-		train_data_gen = ImageDataGenerator(rescale=1. / 255, #revery pixel value from range [0,255] -> [0,1]
-											shear_range=0.2,
-											zoom_range=0.2,
-											rotation_range=45,
-											horizontal_flip=True,
-											vertical_flip=True)
-	else:
-		train_data_gen = ImageDataGenerator(rescale=1. / 255)
+        train_data_gen = ImageDataGenerator(rescale=1. / 255,  # revery pixel value from range [0,255] -> [0,1]
+                                            shear_range=0.2,
+                                            zoom_range=0.2,
+                                            rotation_range=45,
+                                            horizontal_flip=True,
+                                            vertical_flip=True)
+    else:
+        train_data_gen = ImageDataGenerator(rescale=1. / 255)
 
-	valid_data_gen = ImageDataGenerator(rescale=1. / 255)
-	test_data_gen = ImageDataGenerator(rescale=1. / 255)
+    valid_data_gen = ImageDataGenerator(rescale=1. / 255)
+    test_data_gen = ImageDataGenerator(rescale=1. / 255)
 
+    # setup generators
 
-	# setup generators
+    print('\ntrain_gen ... ')
+    train_generator = train_data_gen.flow_from_directory(os.path.join(dataset_split_dir, 'train'),
+                                                         batch_size=bs,
+                                                         target_size=(img_w, img_h),  # images are automatically resized
+                                                         color_mode='rgb',  # read all pictures as rgb
+                                                         classes=class_list,
+                                                         class_mode='categorical',
+                                                         shuffle=True,
+                                                         seed=seed)
 
-	print('\ntrain_gen ... ')
-	train_generator = train_data_gen.flow_from_directory(os.path.join(dataset_split_dir, 'train'),
-												   batch_size=bs,
-												   target_size=(img_w, img_h),  # images are automatically resized
-												   color_mode='rgb',  # read all pictures as rgb
-												   classes=class_list,
-												   class_mode='categorical',
-												   shuffle=True,
-												   seed=seed)
+    print('\nvalid_gen ... ')
+    valid_generator = valid_data_gen.flow_from_directory(os.path.join(dataset_split_dir, 'val'),
+                                                         batch_size=bs,
+                                                         target_size=(img_w, img_h),
+                                                         color_mode='rgb',
+                                                         classes=class_list,
+                                                         class_mode='categorical',
+                                                         shuffle=False,
+                                                         seed=seed)
 
-	print('\nvalid_gen ... ')
-	valid_generator = valid_data_gen.flow_from_directory(os.path.join(dataset_split_dir, 'val'),
-												   batch_size=bs,
-												   target_size=(img_w, img_h),
-												   color_mode='rgb',
-												   classes=class_list,
-												   class_mode='categorical',
-												   shuffle=False,
-												   seed=seed)
+    print('\ntest_gen ... ')
+    # test directory doesn’t have subdirectories the classes of those images are unknown
+    test_generator = test_data_gen.flow_from_directory(dataset_dir,  # specify the parent dir of the test dir
+                                                       batch_size=bs,
+                                                       target_size=(img_w, img_h),
+                                                       color_mode='rgb',
+                                                       classes=['test'],  # load the test “class”
+                                                       # to yield the images in “order”, to predict the outputs
+                                                       # and match them with their unique ids or filenames
+                                                       shuffle=False,
+                                                       seed=seed)
 
+    # todo: create dataset_split.json file indicating how do you split the training set...
 
-	print('\ntest_gen ... ')
-	# test directory doesn’t have subdirectories the classes of those images are unknown
-	test_generator = test_data_gen.flow_from_directory(dataset_dir,  # specify the parent dir of the test dir
-													batch_size=bs,
-													target_size=(img_w, img_h),
-													color_mode='rgb',
-													classes=['test'],  # load the test “class”
-													# to yield the images in “order”, to predict the outputs
-												    # and match them with their unique ids or filenames
-													shuffle=False,
-													seed=seed)
+    # get config params from train generator
+    images, labels = next(train_generator)
 
+    global num_classes, channels
 
-	# todo: create dataset_split.json file indicating how do you split the training set...
+    # inputs (x_train)
+    channels = images.shape[3]
+    print("channels", channels)
 
+    # labels (y_labels)
+    num_classes = labels.shape[1]
+    print("num_classes", num_classes)
 
-	# get config params from train generator
-	images, labels = next(train_generator)
+    return train_generator, valid_generator, test_generator
 
-	global num_classes, channels
-
-	# inputs (x_train)
-	channels = images.shape[3]
-	print("channels", channels)
-
-	# labels (y_labels)
-	num_classes = labels.shape[1]
-	print("num_classes", num_classes)
-
-	return train_generator, valid_generator, test_generator
 
 # Create dataset objects from generators
 # --------------------------------------
 def setup_dataset():
+    # todo: is it necessary to create dataset objects? or work directly with the generators?
+    train_generator, valid_generator, test_generator = setup_data_generator()
 
-	# todo: is it necessary to create dataset objects? or work directly with the generators?
-	train_generator, valid_generator, test_generator = setup_data_generator()
+    train_dataset = dataset_from_generator(train_generator)
+    train_dataset = train_dataset.repeat()
 
-	train_dataset = dataset_from_generator(train_generator)
-	train_dataset = train_dataset.repeat()
-	
-	valid_dataset = dataset_from_generator(valid_generator)
-	valid_dataset = valid_dataset.repeat()
-	
-	test_dataset = dataset_from_generator(test_generator)
-	test_dataset = test_dataset.repeat()
+    valid_dataset = dataset_from_generator(valid_generator)
+    valid_dataset = valid_dataset.repeat()
 
-	return train_dataset, valid_dataset, test_dataset
+    test_dataset = dataset_from_generator(test_generator)
+    test_dataset = test_dataset.repeat()
+
+    return train_dataset, valid_dataset, test_dataset
 
 
 # Create dataset from generator
 # -----------------------
-def dataset_from_generator(generator,img_h, img_w, channels, num_classes):
-	dataset = tf.data.Dataset.from_generator(lambda: generator,
-											 output_types=(tf.float32, tf.float32),
-											 output_shapes=([None, img_h, img_w, channels], [None, num_classes]))
-	return dataset
+def dataset_from_generator(generator, img_h, img_w, channels, num_classes):
+    dataset = tf.data.Dataset.from_generator(lambda: generator,
+                                             output_types=(tf.float32, tf.float32),
+                                             output_shapes=([None, img_h, img_w, channels], [None, num_classes]))
+    return dataset
 
 
 # Iterate Dataset object to access samples inside it
 # --------------------------------------------------
 def show_batch(train_data):
+    import matplotlib.pyplot as plt
+    import numpy as np
 
-	import matplotlib.pyplot as plt
-	import numpy as np
+    iterator = iter(train_data)
+    image_batch, label_batch = next(iterator)
 
-	iterator = iter(train_data)
-	image_batch, label_batch = next(iterator)
+    plt.figure()  # figsize=(10, 10))
 
-	plt.figure() #figsize=(10, 10))
+    # create grid of subplots
+    for i in range(1, 9):
+        plt.subplot(3, 3, i)  # create an axes object in the figure (n_rows, n_cols, plot_id)
 
-	# create grid of subplots
-	for i in range(1, 9):
-		plt.subplot(3, 3, i)  # create an axes object in the figure (n_rows, n_cols, plot_id)
+        # plot raw pixel data
+        image = image_batch[i]  # i-th image
+        image = image * 255  # denormalize
+        plt.imshow(np.uint8(image))
 
-		# plot raw pixel data
-		image = image_batch[i]  # i-th image
-		image = image * 255  # denormalize
-		plt.imshow(np.uint8(image))
+        # label = tf.where(label_batch[i] == 1)
+        # # plt.title(class_list[label])
+        plt.axis('off')
 
-		# label = tf.where(label_batch[i] == 1)
-		# # plt.title(class_list[label])
-		plt.axis('off')
-
-	plt.show()  # show the figure
+    plt.show()  # show the figure
 
 
 def create_keras_model():
-	which_model = 'base_weight_decay'
-	# set_which_model(which_model)  # set model for training_callbacks
+    which_model = 'base_weight_decay'
+    # set_which_model(which_model)  # set model for training_callbacks
 
-	# Create base model using functional API Model (e.g., Input -> Hidden -> Out)
-	if which_model == 'model':
-		# x = tf.keras.Input(shape=[28, 28])  # input tensor
-		# flatten = tf.keras.layers.Flatten()(x)
-		# h = tf.keras.layers.Dense(units=10, activation=tf.keras.activations.sigmoid)(flatten)  # hidden layers
-		# output layer:probabccc of belonging to each class
-		# out = tf.keras.layers.Dense(units=10, activation=tf.keras.activations.softmax)(h)
-		# model = tf.keras.Model(inputs=x, outputs=out)
+    # Create base model using functional API Model (e.g., Input -> Hidden -> Out)
+    if which_model == 'model':
+        # x = tf.keras.Input(shape=[28, 28])  # input tensor
+        # flatten = tf.keras.layers.Flatten()(x)
+        # h = tf.keras.layers.Dense(units=10, activation=tf.keras.activations.sigmoid)(flatten)  # hidden layers
+        # output layer:probabccc of belonging to each class
+        # out = tf.keras.layers.Dense(units=10, activation=tf.keras.activations.softmax)(h)
+        # model = tf.keras.Model(inputs=x, outputs=out)
 
-		# equivalent formulation:
-		model = tf.keras.Sequential()
-		model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))  # or as a list
-		model.add(tf.keras.layers.Dense(units=10, activation=tf.keras.activations.sigmoid))
-		model.add(tf.keras.layers.Dense(units=10, activation=tf.keras.activations.softmax))
+        # equivalent formulation:
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))  # or as a list
+        model.add(tf.keras.layers.Dense(units=10, activation=tf.keras.activations.sigmoid))
+        model.add(tf.keras.layers.Dense(units=10, activation=tf.keras.activations.softmax))
 
-	# Create base model using sequential model (e.g., Input -> Hidden -> Out)
-	elif which_model == 'base':
-		model = tf.keras.Sequential()
-		model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))  # or as a list
-		model.add(tf.keras.layers.Dense(units=1000, activation=tf.keras.activations.sigmoid))
-		model.add(tf.keras.layers.Dense(units=10, activation=tf.keras.activations.softmax))
+    # Create base model using sequential model (e.g., Input -> Hidden -> Out)
+    elif which_model == 'base':
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))  # or as a list
+        model.add(tf.keras.layers.Dense(units=1000, activation=tf.keras.activations.sigmoid))
+        model.add(tf.keras.layers.Dense(units=10, activation=tf.keras.activations.softmax))
 
-	# Create model with Dropout layer
-	elif which_model == 'base_dropout':
+    # Create model with Dropout layer
+    elif which_model == 'base_dropout':
 
-		model = tf.keras.Sequential()
-		model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))  # or as a list
-		model.add(tf.keras.layers.Dense(units=1000, activation=tf.keras.activations.sigmoid))
-		model.add(tf.keras.layers.Dropout(0.3))  # rate (probab): 0.3
-		model.add(tf.keras.layers.Dense(units=10, activation=tf.keras.activations.softmax))
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))  # or as a list
+        model.add(tf.keras.layers.Dense(units=1000, activation=tf.keras.activations.sigmoid))
+        model.add(tf.keras.layers.Dropout(0.3))  # rate (probab): 0.3
+        model.add(tf.keras.layers.Dense(units=10, activation=tf.keras.activations.softmax))
 
-	# Create model with weights penalty (L2 regularization)
-	elif which_model == 'base_weight_decay':
+    # Create model with weights penalty (L2 regularization)
+    elif which_model == 'base_weight_decay':
 
-		model = tf.keras.Sequential()
-		model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))  # or as a list
-		model.add(tf.keras.layers.Dense(units=1000,
-										activation=tf.keras.activations.sigmoid,
-										kernel_regularizer=tf.keras.regularizers.l2(0.0001)))
-		model.add(tf.keras.layers.Dense(units=10,
-										activation=tf.keras.activations.softmax,
-										kernel_regularizer=tf.keras.regularizers.l2(0.0001)))
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))  # or as a list
+        model.add(tf.keras.layers.Dense(units=1000,
+                                        activation=tf.keras.activations.sigmoid,
+                                        kernel_regularizer=tf.keras.regularizers.l2(0.0001)))
+        model.add(tf.keras.layers.Dense(units=10,
+                                        activation=tf.keras.activations.softmax,
+                                        kernel_regularizer=tf.keras.regularizers.l2(0.0001)))
 
-	return model
+    return model
