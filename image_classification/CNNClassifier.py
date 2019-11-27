7516ae596104b385a854d8592391c7a9926dae75
Diego E. Avila <adiego73@gmail.com>
2019-11-27 22:43:27 +0000

    adding transfer learning

--- image_classification/CNNClassifier.py
+++ image_classification/CNNClassifier.py
@@ -45,12 +45,13 @@ class CNNClassifier(tf.keras.Model):
 class ConvBlock(tf.keras.Model):
     def __init__(self, num_filters):
         super(ConvBlock, self).__init__()
-        self.conv2d = tf.keras.layers.Conv2D(filters=num_filters,
-                                             kernel_size=(3, 3),
-                                             strides=(1, 1),
-                                             # padding the input such that the output
-                                             # has the same length as the original input
-                                             padding='same')
+        self.conv2d = tf.keras.layers.Conv2D(
+            filters=num_filters,
+            kernel_size=(3, 3),
+            strides=(1, 1),
+            # padding the input such that the output
+            # has the same length as the original input
+            padding='same')
 
         # we can specify the activation function directly in Conv2D
         self.activation = tf.keras.layers.ReLU()
@@ -59,6 +60,6 @@ class ConvBlock(tf.keras.Model):
     def call(self, inputs):
         x = self.conv2d(inputs)
         x = self.activation(x)
-        x = self.dropout(x)
+        # x = self.dropout(x)
         x = self.pooling(x)
         return x
--- image_classification/transfer_learning.py
+++ image_classification/transfer_learning.py
@@ -1,7 +1,6 @@
 # !/usr/bin/env python3.6
 #  -*- coding utf-8 -*-
 
-
 import tensorflow as tf
 from utils import data_manager as data
 
@@ -10,14 +9,68 @@ from utils import data_manager as data
 seed = 123
 tf.random.set_seed(seed)
 
+# Data loader
+# -----------
+train_generator, valid_generator = data.setup_data_generator()
 
 # User a pre-trained network for transfer learning
 # Load VGG16 model
 # ----------------
 vgg = tf.keras.applications.VGG16(weights='imagenet',
                                   include_top=False,
-                                  input_shape=(data.img_h, data.img_w, data.channels))
+                                  input_shape=(data.img_h, data.img_w,
+                                               data.channels))
 vgg.summary()
 print(vgg.layers)
 
 model_name = 'CNN+TF'
+
+x = vgg.output
+x = tf.keras.layers.GlobalAveragePooling2D()(x)
+# we add dense layers so that the model can learn more complex functions and classify for better results.
+x = tf.keras.layers.Dense(1024, activation='relu')(x)
+# dense layer 2
+x = tf.keras.layers.Dense(1024, activation='relu')(x)
+# dense layer 3
+x = tf.keras.layers.Dense(512, activation='relu')(x)
+# final layer with softmax activation
+preds = tf.keras.layers.Dense(data.num_classes, activation='softmax')(x)
+
+model = tf.keras.Model(inputs=vgg.input, outputs=preds)
+
+for layer in model.layers[:20]:
+    layer.trainable = False
+for layer in model.layers[20:]:
+    layer.trainable = True
+
+model.compile(optimizer='Adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+step_size_train = train_generator.n // train_generator.batch_size
+trained_model = model.fit_generator(generator=train_generator,
+                                    steps_per_epoch=step_size_train,
+                                    epochs=5)
+
+print('\nhistory dict:', trained_model.history)
+
+# Model evaluation
+# ----------------
+# model.load_weights('/path/to/checkpoint')  # use this if you want to restore saved model
+
+eval_out = model.evaluate_generator(valid_generator,
+                                    steps=len(valid_generator),
+                                    verbose=0)
+# test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('eval_out', eval_out)
+
+# Check Performance
+data.visualize_performance(trained_model)
+
+# Generate predictions
+# -------------------
+predictions = input('\nCompute and save predictions?: ' 'y - Yes  n - No\n')
+
+if predictions == 'y':
+    data.generate_predictions(model, model_name)
